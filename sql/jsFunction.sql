18713050421e8f489d31e97cb2217c39f149e999
handstand2002 <gwade@vandals.uidaho.edu>
2016-07-17 23:58:23 -0700

    Added dropping and creating database commands

--- sql/jsFunction.sql
+++ sql/jsFunction.sql
@@ -1,10 +1,14 @@
+DROP DATABASE IF EXISTS webCreator;
+CREATE DATABASE webCreator;
+USE webCreator;
+
 CREATE TABLE jsGroup (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	parentid int NULL,
 	ispage boolean DEFAULT False,
 	
 	FOREIGN KEY (parentid) REFERENCES jsGroup(id)
-)
+);
 
 CREATE TABLE jsFunction (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -12,18 +16,18 @@ CREATE TABLE jsFunction (
 	text blob,
 	
 	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
-)
+);
 
 CREATE TABLE css (
-	groupid int NOT NULL,
+	groupid int NOT NULL PRIMARY KEY,
 	text blob,
 	
 	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
-)
+);
 
 CREATE TABLE html (
-	groupid int NOT NULL,
+	groupid int NOT NULL PRIMARY KEY,
 	text blob,
 	
 	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
-)
+);
