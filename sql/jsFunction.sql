7b7b9ed4891f17e446b2bc75741cc8cdf0a2f399
handstand2002 <gwade@vandals.uidaho.edu>
2016-07-17 20:28:34 -0700

    Added the first tables

---
+++ sql/jsFunction.sql
@@ -0,0 +1,29 @@
+CREATE TABLE jsGroup (
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	parentid int NULL,
+	ispage boolean DEFAULT False,
+	
+	FOREIGN KEY (parentid) REFERENCES jsGroup(id)
+)
+
+CREATE TABLE jsFunction (
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	groupid int NOT NULL,
+	text blob,
+	
+	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
+)
+
+CREATE TABLE css (
+	groupid int NOT NULL,
+	text blob,
+	
+	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
+)
+
+CREATE TABLE html (
+	groupid int NOT NULL,
+	text blob,
+	
+	FOREIGN KEY (groupid) REFERENCES jsGroup(id)
+)
